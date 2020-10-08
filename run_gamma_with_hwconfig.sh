3748b34a6905fb618f1362f5ac70a49c6d0a73aa
Felix <felix@gatech.edu>
2020-10-08 08:25:52 +0800

    Restructrue directory

--- run_gamma_with_hwconfig.sh
+++ run_gamma_with_hwconfig.sh
@@ -1,6 +1,6 @@
 cd ./src/GAMMA
 python main.py --fitness1 latency --fitness2 power --stages 1 --slevel_min 1 --slevel_max 2 --epochs 10 \
-              --model_def vgg16 --singlelayer 1 --num_layer 2 --hwconfig hw_config.m
+              --model vgg16 --singlelayer 1 --hwconfig hw_config.m
 cd ../..
 
 
--- run_others_with_hwconfig.sh
+++ run_others_with_hwconfig.sh
@@ -1,6 +1,6 @@
 cd ./src/Other_opts
 python main.py --fitness1 latency --slevel 1 --epochs 10 \
-              --model_def vgg16 --singlelayer 1 --num_layer 2 --hwconfig hw_config.m --method DE
+              --model vgg16 --singlelayer 1  --hwconfig hw_config.m --method DE
 
 cd ../..
 
